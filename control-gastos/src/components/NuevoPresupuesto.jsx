587f8b3204b53c16de44987a4b53f6ec5a1a84bb
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-04 12:33:47 +0200

    Create presupuesto Form

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
         <form className='formulario'>
             <div className='campo'>
                 <label>Definir presupuesto</label>
-                <input className='nuevo-presupuesto' type="text" placeholder='Agrega tu presupuesto'/>
+                <input 
+                    className='nuevo-presupuesto' 
+                    type="text" 
+                    placeholder='Agrega tu presupuesto' 
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(e.target.value)}
+                />
             </div>
             <input type="submit" value="Agregar" />
         </form>

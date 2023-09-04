e959c6695e3eead9fd91133bf85041a839a3d826
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-04 13:01:24 +0200

    Check presupuesto

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+    
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if(!Number(presupuesto) || Number(presupuesto)) {
+            setMensaje('No es un presupuesto válido')
+        } else {
+            console.log('Si es un presupuesto válido')
+        }
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir presupuesto</label>
                 <input 
@@ -15,6 +28,7 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                 />
             </div>
             <input type="submit" value="Agregar" />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )

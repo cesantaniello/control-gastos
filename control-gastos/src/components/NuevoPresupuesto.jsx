fc83b1241dfb0cc7607505709922e4256770301f
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-04 12:03:41 +0200

    Add Header and NuevoPresupuesto components

---
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NuevoPresupuesto = () => {
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form className='formulario'>
+            <div className='campo'>
+                <label>Definir presupuesto</label>
+                <input className='nuevo-presupuesto' type="text" placeholder='Agrega tu presupuesto'/>
+            </div>
+            <input type="submit" value="Agregar" />
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto

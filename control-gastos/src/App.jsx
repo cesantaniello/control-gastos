ff3fcc79c82c337e9afc71043bc832a469f8f391
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-05 16:25:53 +0200

    Detect gasto when Edit while swiping

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
@@ -13,6 +13,12 @@ function App() {
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0){
+      handleNuevoGasto()}
+  }, [gastoEditar])
 
   const handleNuevoGasto = () => {
     setModal(true)
@@ -46,6 +52,7 @@ function App() {
           <main>
             <ListadoGastos 
               gastos={gastos}
+              setGastoEditar={setGastoEditar}
             />
           </main>
           <div className='nuevo-gasto'>

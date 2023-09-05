be902a3e203bc6f749ed1157c75536ef677184fe
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-05 10:15:54 +0200

    Create gasto component

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -39,13 +40,20 @@ function App() {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto && (
-        <div className='nuevo-gasto'>
-          <img 
-            src={IconoNuevoGasto} 
-            alt="icono nuevo gasto"
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos 
+              gastos={gastos}
+            />
+          </main>
+          <div className='nuevo-gasto'>
+            <img 
+              src={IconoNuevoGasto} 
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+              />
+          </div>
+        </>
       )}
       {modal && 
         <Modal 

8016c0f4225a81b34a9a0abe8ed7a31d103bde44
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-05 18:14:03 +0200

    Reset State when gasto is edit

--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -7,7 +7,8 @@ const Modal = ({
     animarModal, 
     setAnimarModal, 
     guardarGasto, 
-    gastoEditar
+    gastoEditar,
+    setGastoEditar
 }) => {
 
     const [mensaje, setMensaje] = useState('')
@@ -28,7 +29,8 @@ const Modal = ({
     }, [])
 
     const ocultarModal = () => {
-        setAnimarModal(false)   
+        setAnimarModal(false)
+        setGastoEditar({})   
         setTimeout(() => {
             setModal(false)
         }, 500)

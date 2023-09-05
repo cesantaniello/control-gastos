b1dbfea48572666ccbe19dac4ce28a3043430c06
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-05 17:50:07 +0200

    Wipe out Gastos with swipe

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -49,6 +49,11 @@ function App() {
     }, 500)
   }
 
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter(gasto => gasto.id !== id)
+    setGastos(gastosActualizados)
+  }
+
   return (
     <div className={modal ? 'fijar' : ''}>
       <Header
@@ -64,6 +69,7 @@ function App() {
             <ListadoGastos 
               gastos={gastos}
               setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
             />
           </main>
           <div className='nuevo-gasto'>
